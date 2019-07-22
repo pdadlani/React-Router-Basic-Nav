cc2a94f64d563037a4096584e93d0c1135a40654
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-07-22 12:39:52 -0700

    mvp.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { Route, Switch, Link } from "react-router-dom";
+import Home from "./Home.js";
+import About from "./About.js";
+import Contact from "./Contact.js";
+
 
 const Navigation = () => {
   return (
@@ -6,15 +11,20 @@ const Navigation = () => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+          <Link to="/">Home</Link>
         </div>
         <div>
-          <a href="">About</a>
+          <Link to="/about">About</Link>
         </div>
         <div>
-          <a href="">Contact</a>
+          <Link to="/contact">Contact</Link>
         </div>
       </div>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
     </div>
   );
 };
